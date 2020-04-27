7523ba85b784a862d08588b0fcf5ca1aee40389a
Daniela Araujo da Silva <38432420+danicami@users.noreply.github.com>
2020-04-26 21:19:04 -0300

    Add files via upload

---
+++ Sql/dbo.Clientes.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Clientes] (
+    [Id]       INT           IDENTITY (1, 1) NOT NULL,
+    [Nome]     NVARCHAR (50) NULL,
+    [Endereco] NVARCHAR (50) NULL,
+    [Bairro]   NVARCHAR (50) NULL,
+    [CEP]      NVARCHAR (10) NULL,
+    [Cidade]   NVARCHAR (50) NULL,
+    [Estado]   NVARCHAR (50) NULL,
+    [Email]    NVARCHAR(50)    NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
